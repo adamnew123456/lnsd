bbda925c4d01316993bbbd5d267731a73195b2f3
adamnew123456 <adamnew123456@gmail.com>
2014-12-07 17:19:47 -0500

    Completed the control protocol handler, and tests for it.
    This required a number of changes and additions:
    
    - The hostname in a `Host` structure can be `None`, so an explicit check
      had to be added to `Host.unserialize()`
    - The address in an `IP` can also be `None`, so an explicit check had to
      be added to `IP.unserialize()`
    - The `ClientHandler` was added to retrieve information from the server.
    - The `ProtocolHandler` was added to implement the server.

--- lns_ng/control_proto.py
+++ lns_ng/control_proto.py
@@ -22,9 +22,12 @@ A ``QUIT`` message causes the server to terminate.
 from collections import namedtuple
 import json
 import io
+import socket
 import struct
 
-from . import net_proto
+from . import net_proto, reactor, utils
+
+CONTROL_PORT = 10771
 
 def get_length_encoded_json(stream):
     """
@@ -86,14 +89,17 @@ class Host(namedtuple('Host', ['hostname'])):
         if data['type'] != 'name':
             raise ValueError('Got type {}, expected name'.format(data['type']))
 
-        net_proto.verify_hostname(data['hostname'].encode('ascii'))
+        if data['hostname'] is not None:
+            net_proto.verify_hostname(data['hostname'].encode('ascii'))
+
         return Host(data['hostname'])
 
     def serialize(self):
         """
         Produces a bytestring from this message.
         """
-        net_proto.verify_hostname(self.hostname.encode('ascii'))
+        if self.hostname is not None:
+            net_proto.verify_hostname(self.hostname.encode('ascii'))
         return length_encode_json({'type': 'name', 'hostname': self.hostname})
         
 class IP(namedtuple('IP', ['ip'])):
@@ -117,7 +123,8 @@ class IP(namedtuple('IP', ['ip'])):
         if data['type'] != 'ip':
             raise ValueError('Got type {}, expected ip'.format(data['type']))
 
-        verify_ipv4_address(data['ip'])
+        if data['ip'] is not None:
+            verify_ipv4_address(data['ip'])
         return IP(data['ip'])
 
     def serialize(self):
@@ -155,7 +162,7 @@ class GetAll(namedtuple('GetAll', [])):
         """
         return length_encode_json({'type': 'get-all'})
 
-class NameIPMapping(namedtuple('NameIPMapping', ['host_to_names'])):
+class NameIPMapping(namedtuple('NameIPMapping', ['host_to_ips'])):
     TYPE = 'nameipmapping'
 
     @staticmethod
@@ -185,7 +192,7 @@ class NameIPMapping(namedtuple('NameIPMapping', ['host_to_names'])):
         """
         Produces a bytestring from this message.
         """
-        return length_encode_json({'type': 'nameipmapping', 'name_ips': self.host_to_names})
+        return length_encode_json({'type': 'nameipmapping', 'name_ips': self.host_to_ips})
 
 class Quit(namedtuple('Quit', [])):
     TYPE = 'quit'
@@ -227,3 +234,194 @@ def get_message_class(data):
         if msg_class.parses(data):
             return msg_class
     raise ValueError('Could not parse the given dictionary')
+
+class ClientHandler:
+    """
+    Connects to the LNS control port, and sends/retrieves JSON encoded messages
+    from the server.
+    """
+    def __init__(self, port=CONTROL_PORT):
+        self.port = port
+        self.command_sock = None
+
+    def read_json_message(self, expected_message_type):
+        """
+        Reads a JSON message from the socket.
+        """
+        length_header = self.command_sock.recv(2)
+        length = struct.unpack('H', length_header)[0]
+
+        recv_message_raw = b''
+        while len(recv_message_raw) < length:
+            recv_message_raw += self.command_sock.recv(utils.BUFFER_SIZE)
+        
+        json_data = get_length_encoded_json(io.BytesIO(length_header + recv_message_raw))
+        message_class = get_message_class(json_data)
+        if message_class is not expected_message_type:
+            raise ValueError('Expected a {}, but got a {}'.format(
+                expected_message_type, message_class))
+        return message_class.unserialize(json_data)
+
+    def __enter__(self):
+        self.open()
+
+    def __exit__(self, *exc):
+        self.close()
+
+    def open(self):
+        """
+        Opens up the command socket for sending information.
+        """
+        self.command_sock = socket.socket()
+        self.command_sock.connect(('localhost', self.port))
+
+    def close(self):
+        """
+        Closes the command socket.
+        """
+        self.command_sock.close()
+
+    def get_ip(self, host):
+        """
+        Gets the IP address corresponding to a hostname. Note that the return
+        value may be ``None`` if no address corresponds to the given host.
+        """
+        message = Host(host)
+        self.command_sock.send(message.serialize())
+        reply = self.read_json_message(IP)
+        return reply.ip
+
+    def get_host(self, ip):
+        """
+        Gets the host corresponding to an IP address. Note that the return
+        value may be ``None`` if no host corresponds to the given address.
+        """
+        message = IP(ip)
+        self.command_sock.send(message.serialize())
+        reply = self.read_json_message(Host)
+        return reply.hostname
+
+    def get_host_ip_mapping(self):
+        """
+        Gets the entire host -> IP mapping, as a dictionary.
+        """
+        message = GetAll()
+        self.command_sock.send(message.serialize())
+        reply = self.read_json_message(NameIPMapping)
+        return reply.host_to_ips
+
+class ProtocolHandler:
+    """
+    Handles clients on the local machine, which connect to query the host-ip
+    mapping in different ways.
+    """
+    def __init__(self, network_handler, reactor, port=CONTROL_PORT):
+        self.reactor = reactor
+        self.port = port
+        self.server_sock = None
+        self.clients = {}
+        self.client_buffers = {}
+
+        self.network_handler = network_handler
+        self.CLOSE_LIST = {}
+
+    def open(self):
+        """
+        Opens up the command socket for processing clients.
+        """
+        self.server_sock = socket.socket()
+        self.server_sock.bind(('localhost', self.port))
+        self.server_sock.listen(5)
+        self.server_sock.setblocking(False)
+        self.reactor.bind(self.server_sock, reactor.READABLE, self.on_connect)
+
+    def close(self):
+        """
+        Closes the server, as well as any active clients.
+        """
+        to_close = [sock for sock in self.clients.values()]
+        for sock in to_close:
+            self.close_client(sock)
+
+        self.reactor.unbind(self.server_sock)
+        self.server_sock.close()
+
+    def on_connect(self, event):
+        """
+        Handle a connection on the server.
+        """
+        client, _ = self.server_sock.accept()
+        self.clients[client.fileno()] = client
+        self.client_buffers[client.fileno()] = b''
+        self.reactor.bind(client, reactor.READABLE, self.on_message_recv)
+
+    def close_client(self, client_sock):
+        """
+        Closes the client with the given file descriptor, and destroys its
+        buffer.
+        """
+        client_fd = client_sock.fileno()
+        if client_fd == -1:
+            return
+
+        self.reactor.unbind(client_sock)
+        client_sock.close()
+
+        del self.clients[client_fd]
+        del self.client_buffers[client_fd]
+
+    def on_message_recv(self, event):
+        """
+        Handles a message sent by a client.
+        """
+        client_fd, _ = event
+        client_sock = self.clients[client_fd]
+        try:
+            chunk = client_sock.recv(utils.BUFFER_SIZE)
+            if not chunk:
+                self.close_client(client_sock)
+                return
+
+            self.client_buffers[client_fd] += chunk
+            client_buffer_stream = utils.TransactionalBytesIO(self.client_buffers[client_fd])
+            while True:
+                json_message = None
+                with client_buffer_stream.get_transaction() as txn:
+                    txn_stream = txn.get_stream()
+                    try:
+                        json_message = get_length_encoded_json(txn_stream)
+                        txn.commit()
+                    except EOFError:
+                        txn.abort()
+                        break
+
+                if json_message is None:
+                    continue
+
+                message_class = get_message_class(json_message)
+                message = message_class.unserialize(json_message)
+
+                self.handle_message(client_sock, message)
+
+            self.client_buffers[client_fd] = client_buffer_stream.read()
+        except OSError as ex:
+            if client_sock.fileno() != -1:
+                self.close_client(client_sock)
+
+    def handle_message(self, client, message):
+        """
+        Processes the given message which was received from the given client.
+        """
+        reply = None
+        if isinstance(message, Host):
+            ip_addr = self.network_handler.query_host(message.hostname)
+            reply = IP(ip_addr)
+        elif isinstance(message, IP):
+            hostname = self.network_handler.query_ip(message.ip)
+            reply = Host(hostname)
+        elif isinstance(message, GetAll):
+            host_ip_mapping = self.network_handler.get_host_ip_map()
+            reply = NameIPMapping(host_ip_mapping)
+
+        if reply is not None:
+            client.sendall(reply.serialize())
---
+++ test_handlers.py
@@ -0,0 +1,92 @@
+"""
+Ensures that the protocol handlers for the control protocol and the network
+protocol work as expected.
+"""
+import socket
+import threading
+import traceback
+import unittest
+
+from lns_ng import bidict, control_proto, reactor
+
+# Change this to some port that is available on your machine, so that the
+# control protocol handler and the control protocol client can communicate
+TEST_CONTROL_PORT = 4097
+
+# How long to check back with a threading.Event, so that the reactor runner
+# thread can die within a reasonable time
+RUNNER_CHECK_TIME = 2
+
+def reactor_runner_thread(reactor, handler, event):
+    "Steps a reactor, until the given event is triggered."
+    while not event.isSet():
+        reactor.poll(RUNNER_CHECK_TIME)
+    handler.close()
+
+class MockNetworkHandler:
+    """
+    A network handler created to test the control protocol handler, which
+    provides static data for testing.
+    """
+    def __init__(self):
+        self.host_ips = bidict.bidict({'a': '1.2.3.4', 'b': '5.6.7.8'})
+
+    def query_host(self, host):
+        return self.host_ips.get(host, None)
+
+    def query_ip(self, ip):
+        return (~self.host_ips).get(ip, None)
+
+    def get_host_ip_map(self):
+        return dict(self.host_ips)
+
+class TestNetworkProtocol(unittest.TestCase):
+    def setUp(self):
+        self.net_handler = MockNetworkHandler()
+        self.reactor = reactor.Reactor()
+
+        self.control_handler = control_proto.ProtocolHandler(self.net_handler, 
+            self.reactor, port=TEST_CONTROL_PORT)
+        self.control_handler.open()
+
+        self.client = control_proto.ClientHandler(port=TEST_CONTROL_PORT)
+        self.client.open()
+
+        self.reactor_thread_quit_event = threading.Event()
+
+        self.reactor_thread = threading.Thread(target=reactor_runner_thread,
+            args=(self.reactor, self.control_handler, self.reactor_thread_quit_event))
+        self.reactor_thread.setDaemon(True)
+        self.reactor_thread.start()
+
+    def tearDown(self):
+        self.client.close()
+
+        self.reactor_thread_quit_event.set()
+        self.reactor_thread.join()
+
+    def test_query_host(self):
+        """
+        Queries a known and an unknown host name.
+        """
+        self.assertEqual(self.client.get_ip('a'), '1.2.3.4')
+        self.assertEqual(self.client.get_ip('b'), '5.6.7.8')
+        self.assertEqual(self.client.get_ip('nonexistent'), None)
+
+    def test_query_ip(self):
+        """
+        Queries a known and an unknown IP address.
+        """
+        self.assertEqual(self.client.get_host('1.2.3.4'), 'a')
+        self.assertEqual(self.client.get_host('5.6.7.8'), 'b')
+        self.assertEqual(self.client.get_host('0.0.0.0'), None)
+
+    def test_host_ip_mapping(self):
+        """
+        Queries the mapping from hostnames to IP addresses.
+        """
+        self.assertEqual(self.client.get_host_ip_mapping(), 
+            {'a': '1.2.3.4', 'b': '5.6.7.8'})
+
+if __name__ == '__main__':
+    unittest.main()
