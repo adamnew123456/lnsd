12b9ee7fa48e185146519971a4f53eef266d0cf2
adamnew123456 <adamnew123456@gmail.com>
2014-12-09 18:15:26 -0500

    Implemented the network protocol handler.

--- lns_ng/net_proto.py
+++ lns_ng/net_proto.py
@@ -6,19 +6,25 @@ This implements the network-facing side of lnsd. There are only two types of
 messages, ``ANNOUNCE`` and ``CONFLICT``.
 
 An ``ANNOUNCE`` packet is sent periodically to ensure that a host is still on a
-network, and that it is asserting a particular hostname. If another host 
-discovers that there is a name clash, it sends a ``CONFLICT`` to the host with
-the duplicate name. When a host receives a ``CONFLICT`` packet, the host should
-shut down its server until the name clash can be resolved.
-
-In the indefinite future, I plan to remove ``CONFLICT`` from the protocol, and
-simply allow lnsd to tolerate hosts with the same name.
+network, and that it is asserting a particular hostname. Other hosts should
+record this message as they receive it, to update their caches.
 """
-from collections import namedtuple
+from collections import defaultdict, namedtuple
+import socket
+import time
+
+from . import reactor, utils
+
+NET_PORT = 15051
 
 # All packets received from the network are 512 bytes, not including the UDP
 # header
 PACKET_SIZE = 512
+
+# How often to Announce to the the network, in seconds
+ANNOUNCE_ALARM = 10
+# How long to wait for another host to Announce before we drop it
+ANNOUNCE_TTL = 30
 
 def verify_hostname(hostname_bytes):
     """
@@ -81,47 +87,111 @@ class Announce(namedtuple('Announce', ['hostname'])):
 
         return b'\x01' + raw_hostname.ljust(PACKET_SIZE - 1, b'\x00')
 
-class Conflict(namedtuple('Conflict', [])):
-    HEADER = 0x02
-
-    @staticmethod
-    def parses(buffer):
-        """
-        Returns ``True`` if this class can parse the given buffer, or
-        ``False`` otherwise.
-        """
-        return buffer and buffer[0] == Conflict.HEADER
-
-    @staticmethod
-    def unserialize(buffer):
-        """
-        Produces a :class:`Conflict` message from the contents of a buffer.
-        """
-        if len(buffer) != PACKET_SIZE:
-            raise ValueError('Packet not the correct length - '
-                '{} bytes, expected {}'.format(len(buffer, PACKET_SIZE)))
-
-        header = buffer[0]
-        if header != Conflict.HEADER:
-            raise ValueError('Header byte incorrect - got {}, expected 0x02'.format(
-                hex(header)))
-
-        return Conflict()
-
-    def serialize(self):
-        """
-        Produces a bytestring from this message.
-        """
-        return b'\x02'.ljust(PACKET_SIZE, b'\x00')
-
-MESSAGE_CLASSES = {Announce, Conflict}
-def get_message_class(buffer):
+class ProtocolHandler:
     """
-    Gets the message class capable of parsing the given buffer.
-
-    :raises ValueError: If the given message cannot be handled by any class.
+    Handles remote LNS servers, sending out Announce messages and caching them,
+    while periodically sending out its own Announce message.
     """
-    for msg_class in MESSAGE_CLASSES:
-        if msg_class.parses(buffer):
-            return msg_class
-    raise ValueError('Could not parse the given buffer')
+    def __init__(self, reactor, port=NET_PORT):
+        self.reactor = reactor
+        self.port = port
+        self.server_sock = None
+
+        self.last_ping_time = None
+        self.peer_last_ping_time = {}
+        self.host_to_ips = defaultdict(set)
+        self.ips_to_hosts = {}
+        self.peer_buffers = defaultdict(bytes)
+
+    def open(self):
+        """
+        Opens up the network socket for sending and receiving Announce messages.
+        """
+        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.server_sock.bind(('255.255.255.255', self.port))
+        self.reactor.bind(self.server_sock, reactor.READABLE, self.on_message)
+        self.reactor.add_step_callback(self.on_announce_timeout)
+        
+        # Go ahead and do our first announce, so that we appear on the network ASAP
+        self.on_announce_timeout()
+
+    def query_host(self, host):
+        """
+        Gets a list of IP addresses which have the given hostname.
+        """
+        return list(self.host_to_ips[host])
+
+    def query_ip(self, ip):
+        """
+        Gets a host for the given IP address, or None.
+        """
+        return self.ips_to_hosts.get(ip, None)
+
+    def get_host_ip_map(self):
+        """
+        Gets the host to IP address map.
+        """
+        return {host: ips for host, ips in self.host_to_ips if ips}
+
+    def on_announce_timeout(self):
+        """
+        This sends out a new announce message, and drops any clients whose last
+        announce was too far back in time.
+        """
+        utils.sendto_all(self.server_sock, Announce().serialize(), 
+            ('255.255.255.255', self.port))
+        self.last_ping_time = time.time()
+
+        now = time.time()
+        to_drop = [ peer 
+            for peer, last_ping_time in self.peer_last_ping_time.items()
+            if now - last_ping_time > ANNOUNCE_TTL
+        ]
+            
+        for peer in to_drop:
+            del self.peer_last_ping_time[peer]
+            del self.peer_buffers[peer]
+
+            peer_name = self.ips_to_hosts[peer]
+            del self.ips_to_hosts[peer]
+            self.host_to_ips[peer].remove(peer_name)
+
+    def get_time_until_next_ping(self):
+        """
+        Gets the amount of time since the last ping.
+        """
+        time_since_last_ping = time.time() - self.last_ping_time
+        return max(ANNOUNCE_ALARM - time_since_last_ping, 0)
+
+    def handle_messages(self, host):
+        """
+        Handles any messages which exist for the given peer.
+        """
+        buffer_stream = utils.TransactionalBytesIO(self.peer_buffers[host])
+        while True:
+            with buffer_stream.get_transaction() as txn:
+                stream = txn.get_stream()
+                packet = stream.read(PACKET_SIZE)
+                if len(packet) < PACKET_SIZE:
+                    # We've found the last partial packet, which we want to
+                    # keep in the buffer - since we've read it, we have to
+                    # abort the transaction to restore its state
+                    txn.abort()
+                    break
+                txn.commit()
+
+                if not Announce.parses(packet):
+                    # A corrupt Announce packet (or some other kind of data we
+                    # cannot use) is best ignored, since we can't do anything
+                    continue
+                self.peer_last_ping_time[host] = time.time()
+
+    def on_message(self):
+        """
+        Retrieves data from the server socket, and saves it into the peer's buffer,
+        possibly handling any complete messages in that peer's buffer.
+        """
+        data, (host, port) = self.server_sock.recvfrom(PACKET_SIZE)
+        self.peer_buffers[host] += data
+        self.handle_messages(host)
